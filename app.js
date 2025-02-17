ee6b2b352f649074ca7ae0192ae157dbb03a14f4
Beatriz Ebert <ebert.beatriz.be@gmail.com>
2025-02-16 22:03:35 -0300

    desarrollando

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const input = document.getElementById("amigo");
+if (input === null || input === "") {
+    alert ("No ha escrito ningun valor, por favor escriba nuevamente");
+} else if (input.length <= 3) {
+    alert ("El nombre ingresado es muy corto, ingrese nuevamente el nombre");
+} else if (input === ) {
+
+}
+
+function sortearLibro() {
+    let libroElegido = parseInt(Math.random() * numeroLimite + 1);
+    let cantidadDeLibrosSorteados = listaDeLibrosSorteados.length;
+    if (cantidadDeLibrosSorteados == numeroLimite) {
+        listaDeLibrosSorteados = [];
+    }
+}
